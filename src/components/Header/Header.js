444fd26e210c4bdf4dcc8e51de2c8fc71b079cc5
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-07-16 14:13:42 -0500

    ADD new section Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="seplom-header">
+      <h1>
+        <span>Plomeria</span>
+        <br />
+        <span>Inmediata.</span>
+      </h1>
+      <button className="btn btn-contratar">Contratar</button>
+      <button className="btn btn-cotizacion">Cotizacion</button>
+      <p className="seplom-header-paragraph">
+        Seplom es una empresa de <b>Serivicio de Plomeria Inmediata</b> para
+        ayudarte en los 
+        <br />
+        trabajos que necesites a un precio accesible para todo
+        el publico
+      </p>
+    </header>
+  );
+};
+
+export default Header;
--- src/container/App.js
+++ src/container/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Navbar } from "../components";
 import Promotion from "../components/Promotion/Promotion";
+import Header from "../components/Header/Header";
 
 const App = () => {
   return (
     <>
       <Promotion />
       <Navbar />
+      <Header />
     </>
   );
 };
