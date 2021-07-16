009cbba7ed2fe92d507d395cba79679d7c2ccf89
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-07-16 15:07:24 -0500

    ADD footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import './Footer.css'
+import React from "react";
+import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
+import Logo from "../../assets/static/seplom-logo.png";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer>
-            
-        </footer>
-    );
+  return (
+    <footer className="seplom-footer">
+      <div className="footer-top">
+        <ul>
+          <b>Nosotros</b>
+          <li>Politicas Privacidad</li>
+          <li>Reglas de Uso</li>
+          <li>Cotizacion</li>
+          <li>Rembolso</li>
+          <li>Garantia</li>
+        </ul>
+        <ul>
+          <b>Servicios</b>
+          <li>Plomeria</li>
+          <li>Precios</li>
+          <li>Cotizacion</li>
+          <li>Contactos</li>
+          <li>Unirse</li>
+        </ul>
+        <ul>
+          <b>Servicios</b>
+          <li>Plomeria</li>
+          <li>Precios</li>
+          <li>Cotizacion</li>
+          <li>Contactos</li>
+          <li>Unirse</li>
+        </ul>
+        <ul>
+          <b>Company</b>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>Seplom Conf</li>
+          <li>Parthers</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div className="footer-bot">
+      <img className="logo-footer" src={Logo} alt="Logo Seplom" />
+
+        <p>Copyright © 2021 Seplom Inc. All rights reserved</p>
+        <input
+          className="feedback-btn"
+          type="text"
+          placeholder="Enviar Feedback |"
+        />
+        <div className="footer-bot-icons">
+          <AiFillFacebook />
+          <AiFillInstagram />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/container/App.js
+++ src/container/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import "./App.css";
-import { Navbar } from "../components";
-import Promotion from "../components/Promotion/Promotion";
-import Header from "../components/Header/Header";
+import React from 'react';
+import './App.css'
+
+import Promotion from '../components/Promotion/Promotion'
+import Navbar from '../components/Navbar/Navbar'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 const App = () => {
   return (
-    <>
+    <div>
       <Promotion />
       <Navbar />
       <Header />
-    </>
+      <Footer />
+    </div>
   );
 };
 
