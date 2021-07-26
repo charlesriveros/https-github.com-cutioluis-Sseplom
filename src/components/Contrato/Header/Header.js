4217e6c9cb7f6c09ee11ea5caf03cb29bc26aa1a
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-07-26 14:09:08 -0500

    ADD contrato component

---
+++ src/components/Contrato/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="seplom-header-title">Contrata Seplom</h1>
+      <p>
+        Contrata Seplom en <span>3 Simples Pasos</span>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
--- src/container/Contrato/Contrato.js
+++ src/container/Contrato/Contrato.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../../components/Contrato/Header/Header';
+import Form from '../../components/Contrato/Form/Form'
 
 const Contrato = () => {
     return (
         <div>
-            
+            <Header />
+            <Form />
         </div>
     );
 };
